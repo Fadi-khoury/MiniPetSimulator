ce2863519aefedef31fcdb7c7a2ac4664292451b
FADI <fadialkhouri.820@gmail.com>
2026-04-15 22:28:57 +0300

    add feed_pet feature

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from pet import Pet
 my_pet = Pet("Buddy", 50)
 print(f"Pet name: {my_pet.name}")
 print(f"Energy level: {my_pet.energy_level}")
+
+my_pet.feed_pet()
--- pet.py
+++ pet.py
@@ -2,3 +2,7 @@ class Pet:
     def init(self, name, energy_level):
         self.name = name
         self.energy_level = energy_level
+
+    def feed_pet(self):
+        self.energy_level += 10
+        print(f"{self.name} is fed! Energy level is now {self.energy_level}")
