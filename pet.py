58810dc5a0f15230957d80d7bf465b89da0e2ef7
FADI <fadialkhouri.820@gmail.com>
2026-04-15 22:33:45 +0300

    add play_with_pet feature

--- pet.py
+++ pet.py
@@ -2,3 +2,7 @@ class Pet:
     def init(self, name, energy_level):
         self.name = name
         self.energy_level = energy_level
+
+    def play_with_pet(self):
+        self.energy_level -= 10
+        print(f"{self.name} played! Energy level is now {self.energy_level}")
